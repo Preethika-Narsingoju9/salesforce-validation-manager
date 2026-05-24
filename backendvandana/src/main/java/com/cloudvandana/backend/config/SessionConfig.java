a0cd40cdb91b1f981ff69fac6e905bc39e17cfbc
Preethika Narsingoju <navyaasuresh009@gmail.com>
2026-05-24 17:06:49 +0530

    final fix code

--- backendvandana/src/main/java/com/cloudvandana/backend/config/SessionConfig.java
+++ backendvandana/src/main/java/com/cloudvandana/backend/config/SessionConfig.java
@@ -1,47 +1,4 @@
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class SessionConfig {
-
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                         .allowedOrigins("https://salesforce-frontend-41ny.onrender.com")
-//                         .allowedMethods("*")
-//                         .allowCredentials(true);
-//             }
-//         };
-//     }
-// }
-
-
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// @Configuration
-// public class SessionConfig {
-
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                         .allowedOrigins("https://salesforce-frontend-41ny.onrender.com")
-//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                         .allowCredentials(true);
-//             }
-//         };
-//     }
-// }
 
 
 package com.cloudvandana.backend.config;
